4d12494cb6ce6646ecdf11ceaccd58c549d0d6b3
DJ-Programmer-Micho <michelshabo1@gmail.com>
2024-09-30 19:51:01 +0300

    switch back

--- resources/views/mains/pages2/Register-page-one.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('mains.layout.app')
-@section('business-content')
-<div class="main">
-    <x-mains.components.register.image-header-one />
-    <x-mains.components.register.nav-one />
-    <x-mains.components.register.dashboard />
-</div>
-@endsection
--- resources/views/mains/pages2/wishlist-page-one.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('mains.layout.app')
-@section('business-content')
-<div class="main">
-    <x-mains.components.account.image-header-one />
-    <x-mains.components.account.nav-one />
-    @livewire('cart.wishlist-list-livewire')
-</div>
-@endsection
