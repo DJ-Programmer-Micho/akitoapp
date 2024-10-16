7b6f2204ff99d1ba7085db48242bb0cc4fb5dfd2
DJ-Programmer-Micho <michelshabo1@gmail.com>
2024-10-16 16:07:03 +0300

    vite update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0', // Allow access from any IP address
+        port: 5173,      // Default port for Vite
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
