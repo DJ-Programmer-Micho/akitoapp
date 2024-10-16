64c2309d16bd21ae7116dff0e59b6f5e5bd2efd4
DJ-Programmer-Micho <michelshabo1@gmail.com>
2024-10-16 16:10:55 +0300

    update vite https

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
     server: {
         host: '0.0.0.0', // Allow access from any IP address
         port: 5173,      // Default port for Vite
+        https: true, // Serve over HTTPS
     },
     plugins: [
         laravel({
